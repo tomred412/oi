fc86f1801999a7db12713a01e36c1a39b9b9f535
tomred412 <118136270+tomred412@users.noreply.github.com>
2025-01-31 19:00:04 -0500

    Create tree.cpp

---
+++ tree/tree.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+  int data;
+  int left, right;
+};
+
+Node s[10000];
+int size = 1;
+int root = 0;
+
+void insert(int data) {
+
+  if (root == -1) { 
+        root = 0;
+        s[root].data = data;
+        s[root].left = -1;
+        s[root].right = -1;
+        return;
+    }
+
+  int current = root;
+
+  while (true) {
+    if (data > s[current].data) {
+      if (s[current].right == -1) {
+        s[current].right = size;
+        s[size].left = -1;
+        s[size].right = -1;
+        s[size].data = data;
+        size++;
+        return;
+      }
+      current = s[current].right;
+      continue;
+    }
+    if (data < s[current].data) {
+      if (s[current].left == -1) {
+        s[current].left = size;
+        s[size].left = -1;
+        s[size].right = -1;
+        s[size].data = data;
+        size++;
+        return;
+      }
+      current = s[current].left;
+      continue;
+    }
+  }
+}
+
+int main() {
+
+int n;
+while (cin >> n) {
+  insert(n);
+}
+
+  return 0;
+}
