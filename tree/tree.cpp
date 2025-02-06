7b4a642f8262b6799084cb828259109f33be97de
tomred412 <118136270+tomred412@users.noreply.github.com>
2025-02-06 16:15:33 -0500

    Update tree.cpp

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -12,14 +12,13 @@ int size = 1;
 int root = -1;
 
 void insert(int data) {
-
-  if (root == -1) { 
-        root = 0;
-        s[root].data = data;
-        s[root].left = -1;
-        s[root].right = -1;
-        return;
-    }
+  if (root == -1) {
+    root = 0;
+    s[root].data = data;
+    s[root].left = -1;
+    s[root].right = -1;
+    return;
+  }
 
   int current = root;
 
@@ -51,12 +50,63 @@ void insert(int data) {
   }
 }
 
-int main() {
+void print();
+// prints the data in the tree, in order
 
-int n;
-while (cin >> n) {
-  insert(n);
+void print(int node) {
+  if (s[node].left != -1) {
+    print(s[node].left);
+  }
+
+  cout << s[node].data << " ";
+
+  if (s[node].right != -1) {
+    print(s[node].right);
+  }
 }
+
+bool find(int data) {
+  int current = root;
+
+  while (current != -1) {
+    if (s[current].data == data) return true;
+    if (data < s[current].data) current = s[current].left;
+    if (data > s[current].data) current = s[current].right;
+  }
+
+  return false;
+}
+
+int main() {
+  cout << "Please enter the list, then a period, then m numbers to find in the "
+          "tree.\n";
+
+  int n;
+  while (cin >> n) {
+    insert(n);
+    if (cin.peek() == '.') break;
+  }
+
+  char period;
+  cin >> period;
+
+  if (period != '.') {
+    cout << "There should be a period.\n";
+    return 0;
+  }
+
+  int m;
+  cin >> m;
+
+  int p = 0;
+
+  for (int i = 0; i < m; i++) {
+    cin >> p;
+    if (find(p) == true) cout << p << ": yes\n";
+    if (find(p) == false) cout << p << ": no\n";
+  }
+  cout << "Sorted tree:\n";
+  print(0);
 
   return 0;
 }
